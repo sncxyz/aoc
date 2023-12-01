a552931ac76aedb51bf1d2dcec674658924366f9
Sam Collins <samc3259@gmail.com>
2023-12-01 11:11:09 +0000

    2023 01 update function names

--- 2023/01/src/main.rs
+++ 2023/01/src/main.rs
@@ -9,11 +9,11 @@ const DIGITS: &[&str] = &[
 fn part_1(input: Input) -> u32 {
     input
         .lines()
-        .map(|line| find_num(line.chars()) * 10 + find_num(line.chars().rev()))
+        .map(|line| find_1(line.chars()) * 10 + find_1(line.chars().rev()))
         .sum()
 }
 
-fn find_num(mut iter: impl Iterator<Item = char>) -> u32 {
+fn find_1(mut iter: impl Iterator<Item = char>) -> u32 {
     iter.find_map(|c| c.to_digit(10)).unwrap()
 }
 
@@ -21,12 +21,12 @@ fn part_2(input: Input) -> u32 {
     input
         .lines()
         .map(|line| {
-            find_digit(line, line.char_indices()) * 10 + find_digit(line, line.char_indices().rev())
+            find_2(line, line.char_indices()) * 10 + find_2(line, line.char_indices().rev())
         })
         .sum()
 }
 
-fn find_digit(line: &str, mut iter: impl Iterator<Item = (usize, char)>) -> u32 {
+fn find_2(line: &str, mut iter: impl Iterator<Item = (usize, char)>) -> u32 {
     iter.find_map(|(i, c)| {
         c.to_digit(10).or_else(|| {
             DIGITS
