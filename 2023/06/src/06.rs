205d21c336ef5c6800eafc954f8ae2b5b7c98f33
Sam Collins <samc3259@gmail.com>
2023-12-06 13:07:25 +0000

    2023 06

---
+++ 2023/06/src/06.rs
@@ -0,0 +1,39 @@
+use aoc::Parse;
+
+aoc::parts!(1, 2);
+
+fn part_1(input: aoc::Input) -> u64 {
+    input[0][11..]
+        .ints_iter()
+        .zip(input[1][11..].ints_iter())
+        .map(range)
+        .product()
+}
+
+fn part_2(input: aoc::Input) -> u64 {
+    range((parse_2(input[0]), parse_2(input[1])))
+}
+
+#[inline(always)]
+fn parse_2(line: &str) -> u64 {
+    line[11..]
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .map(|d| d as u64)
+        .reduce(|x, d| x * 10 + d)
+        .unwrap()
+}
+
+fn range((time, dist): (u64, u64)) -> u64 {
+    let mut min = 1;
+    let mut max = time / 2;
+    while min < max {
+        let mid = (min + max) / 2;
+        if mid * (time - mid) <= dist {
+            min = mid + 1;
+        } else {
+            max = mid;
+        }
+    }
+    time - 2 * min + 1
+}
