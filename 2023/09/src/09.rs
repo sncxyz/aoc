a0b170e0160f71fdbb67b8a368be8913e5bfd7a9
Sam Collins <samc3259@gmail.com>
2023-12-09 10:04:04 +0000

    2023 09

---
+++ 2023/09/src/09.rs
@@ -0,0 +1,34 @@
+use aoc::Parse;
+
+aoc::parts!(1, 2);
+
+fn part_1(input: aoc::Input) -> i32 {
+    input.lines().map(forwards).sum()
+}
+
+fn part_2(input: aoc::Input) -> i32 {
+    input.lines().map(backwards).sum()
+}
+
+fn forwards(line: &str) -> i32 {
+    let mut values: Vec<i32> = line.ints_iter().collect();
+    for limit in (0..values.len()).rev() {
+        for i in 0..limit {
+            values[i] = values[i + 1] - values[i];
+        }
+    }
+    values.into_iter().sum()
+}
+
+fn backwards(line: &str) -> i32 {
+    let mut values: Vec<i32> = line.ints_iter().collect();
+    for limit in 1..values.len() {
+        for i in (limit..values.len()).rev() {
+            values[i] -= values[i - 1];
+        }
+        if limit % 2 == 0 {
+            values[limit - 1] = -values[limit - 1];
+        }
+    }
+    values.into_iter().sum()
+}
