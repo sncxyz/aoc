f046a85cdd6062ab2f4899ae882bbafa0c9d4006
Sam Collins <samc3259@gmail.com>
2023-12-13 00:51:10 +0000

    polish 2023 12

--- 2023/12/src/12.rs
+++ 2023/12/src/12.rs
@@ -7,23 +7,20 @@ fn part_1(input: aoc::Input) -> u64 {
 }
 
 fn part_2(input: aoc::Input) -> u64 {
-    input
-        .lines()
-        .map(Row::parse)
-        .map(Row::unfold)
-        .map(Row::solve)
-        .sum()
+    input.lines().map(Row::parse_unfold).map(Row::solve).sum()
 }
 
 struct Row {
     springs: Vec<Spring>,
     groups: Vec<usize>,
+    damaged: usize,
 }
 
 impl Row {
     fn parse(line: &str) -> Self {
         let (row, groups) = line.split_once(' ').unwrap();
 
+        let mut damaged = 0;
         let mut springs = Vec::new();
         let mut operational = false;
         for byte in row.bytes() {
@@ -34,49 +31,46 @@ impl Row {
                     operational = true;
                 }
             } else {
-                operational = false;
+                if spring == Spring::Damaged {
+                    damaged += 1;
+                }
                 springs.push(spring);
+                operational = false;
             }
         }
 
         let groups = groups.split(',').map(Parse::parse_uw).collect();
 
-        Self { springs, groups }
+        Self {
+            springs,
+            groups,
+            damaged,
+        }
     }
 
-    fn unfold(mut self) -> Self {
-        self.springs = vec![self.springs; 5].as_slice().join(&Spring::Unknown);
-        self.groups = self.groups.repeat(5);
-        self
+    fn parse_unfold(line: &str) -> Self {
+        let mut row = Self::parse(line);
+        row.springs = vec![row.springs; 5].as_slice().join(&Spring::Unknown);
+        row.groups = row.groups.repeat(5);
+        row.damaged *= 5;
+        row
     }
 
     fn solve(self) -> u64 {
         let total_springs = self.springs.len();
-        let mut total_damaged = 0;
-        for &spring in &self.springs {
-            if spring == Spring::Damaged {
-                total_damaged += 1;
-            }
-        }
 
-        let mut last = Vec::with_capacity(total_springs);
-        let mut add = vec![0; total_damaged + 1];
-        for i in 0..total_springs {
-            if let Some(ds) = self.group_at(i, self.groups[0]) {
-                add[ds] += 1;
-            }
-            last.push(add.clone());
-        }
+        let mut last_group = 0;
+        let mut first = vec![0; self.damaged + 1];
+        first[0] = 1;
+        let mut last = vec![first; total_springs];
 
-        let mut last_group = self.groups[0] + 1;
-
-        for &group in &self.groups[1..] {
+        for &group in &self.groups {
             let mut next = Vec::with_capacity(total_springs);
-            let mut add = vec![0; total_damaged + 1];
+            let mut add = vec![0; self.damaged + 1];
             for i in 0..total_springs {
                 if i >= last_group {
                     if let Some(ds) = self.group_at(i, group) {
-                        for d in 0..total_damaged + 1 {
+                        for d in 0..=self.damaged {
                             if last[i - last_group][d] > 0 {
                                 add[d + ds] += last[i - last_group][d];
                             }
@@ -89,7 +83,7 @@ impl Row {
             last_group = group + 1;
         }
 
-        last[total_springs - 1][total_damaged]
+        last[total_springs - 1][self.damaged]
     }
 
     fn group_at(&self, i: usize, g: usize) -> Option<usize> {
@@ -104,11 +98,10 @@ impl Row {
         }
         let mut count = 0;
         for &spring in &self.springs[i..i + g] {
-            if spring == Spring::Operational {
-                return None;
-            }
-            if spring == Spring::Damaged {
-                count += 1;
+            match spring {
+                Spring::Operational => return None,
+                Spring::Damaged => count += 1,
+                _ => (),
             }
         }
         Some(count)
@@ -130,31 +123,5 @@ impl Spring {
             b'?' => Self::Unknown,
             _ => unreachable!(),
         }
-    }
-}
-
-use std::fmt;
-
-impl fmt::Debug for Spring {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Operational => write!(f, "."),
-            Self::Damaged => write!(f, "#"),
-            Self::Unknown => write!(f, "?"),
-        }
-    }
-}
-
-impl fmt::Debug for Row {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for spring in &self.springs {
-            write!(f, "{spring:?}")?;
-        }
-        write!(f, " ")?;
-        write!(f, "{}", self.groups[0])?;
-        for group in &self.groups[1..] {
-            write!(f, ",{group}")?;
-        }
-        Ok(())
     }
 }
