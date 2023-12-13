1697a4b019fd6e79004a7d556bd34b1aaf1d508d
Sam Collins <samc3259@gmail.com>
2023-12-13 14:07:59 +0000

    2023 13 optimise

--- 2023/13/src/13.rs
+++ 2023/13/src/13.rs
@@ -8,12 +8,18 @@ fn part_2(input: aoc::Input) -> impl ToString {
     part_n(input, solve_2)
 }
 
-fn part_n(input: aoc::Input, solve: fn(u32, (Vec<u32>, Vec<u32>)) -> u32) -> u32 {
+fn part_n(input: aoc::Input, solve: fn(Vec<u32>) -> Option<u32>) -> u32 {
     input
         .as_lines()
         .split(|line| line.is_empty())
         .map(parse)
-        .fold(0, solve)
+        .map(|(rows, cols)| {
+            solve(rows)
+                .map(|r| r * 100)
+                .or_else(|| solve(cols))
+                .unwrap()
+        })
+        .sum()
 }
 
 fn parse(lines: &[&str]) -> (Vec<u32>, Vec<u32>) {
@@ -34,49 +40,44 @@ fn parse(lines: &[&str]) -> (Vec<u32>, Vec<u32>) {
     (rows, cols)
 }
 
-fn solve_1(total: u32, (rows, cols): (Vec<u32>, Vec<u32>)) -> u32 {
-    total
-        + get_reflection(&rows, None)
-            .map(|r| r * 100)
-            .or_else(|| get_reflection(&cols, None))
-            .unwrap()
-}
-
-fn solve_2(total: u32, (mut rows, mut cols): (Vec<u32>, Vec<u32>)) -> u32 {
-    let prev_row = get_reflection(&rows, None);
-    let prev_col = get_reflection(&cols, None);
-
-    for row in 0..rows.len() {
-        for col in 0..cols.len() {
-            rows[row] ^= 1 << col;
-            cols[col] ^= 1 << row;
-            if let Some(r) = get_reflection(&rows, prev_row) {
-                return total + 100 * r;
-            } else if let Some(r) = get_reflection(&cols, prev_col) {
-                return total + r;
-            }
-            rows[row] ^= 1 << col;
-            cols[col] ^= 1 << row;
-        }
-    }
-
-    unreachable!()
-}
-
-fn get_reflection(lines: &[u32], ignore: Option<u32>) -> Option<u32> {
+fn solve_1(lines: Vec<u32>) -> Option<u32> {
     for i in 1..lines.len() {
         let max = i.min(lines.len() - i);
         let mut is_reflection = true;
         for j in 0..max {
             if lines[i + j] != lines[i - j - 1] {
                 is_reflection = false;
+                break;
             }
         }
         if is_reflection {
-            let r = Some(i as u32);
-            if r != ignore {
-                return r;
+            return Some(i as u32);
+        }
+    }
+    None
+}
+
+fn solve_2(lines: Vec<u32>) -> Option<u32> {
+    for i in 1..lines.len() {
+        let max = i.min(lines.len() - i);
+        let mut is_reflection = true;
+        let mut found = false;
+        for j in 0..max {
+            let (a, b) = (lines[i + j], lines[i - j - 1]);
+            if (a ^ b).count_ones() == 1 {
+                if found {
+                    is_reflection = false;
+                    break;
+                } else {
+                    found = true;
+                }
+            } else if a != b {
+                is_reflection = false;
+                break;
             }
+        }
+        if is_reflection && found {
+            return Some(i as u32);
         }
     }
     None
