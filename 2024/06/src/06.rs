9e308e780a9a816c3b208c1511ea1bc9b4ab1f02
Sam Collins <samc3259@gmail.com>
2024-12-06 13:44:37 +0000

    2024 06 improved

--- 2024/06/src/06.rs
+++ 2024/06/src/06.rs
@@ -28,17 +28,28 @@ fn part_1(input: aoc::Input) -> impl ToString {
 
 fn part_2(input: aoc::Input) -> impl ToString {
     let mut map = Matrix::new(input.lines().map(|line| line.bytes()));
+    let start = map.enumerate().find(|(_, x)| **x == b'^').unwrap().0;
+    map[start] = b'.';
+
+    let mut candidates = HashSet::default();
+    let mut pos = start;
+    let mut dir = Vector::n();
+    while let Some(&x) = map.get(pos + dir) {
+        if x == b'#' {
+            dir = dir.perp();
+        } else {
+            pos += dir;
+            candidates.insert(pos);
+        }
+    }
+    candidates.remove(&start);
+
     let dim: Vector = map.dim();
-    let mut start = None;
 
     let mut up = Matrix::init(dim, None);
     for x in 0..map.width() {
         let mut dest: Option<Vector> = None;
         for y in 0..map.height() {
-            if map[v(x, y)] == b'^' {
-                map[v(x, y)] = b'.';
-                start = Some(v(x, y));
-            }
             if map[v(x, y)] == b'#' {
                 dest = Some(v(x, y + 1));
             } else {
@@ -46,8 +57,6 @@ fn part_2(input: aoc::Input) -> impl ToString {
             }
         }
     }
-
-    let start = start.unwrap();
 
     let mut right = Matrix::init(dim, None);
     for y in 0..map.height() {
@@ -87,112 +96,21 @@ fn part_2(input: aoc::Input) -> impl ToString {
 
     let dests = [up, right, down, left];
 
-    let mut to_check = HashSet::default();
-    let mut pos = start;
-    let mut dir = 0;
-    loop {
-        if let Some(dest) = dests[dir][pos] {
-            match dir {
-                0 => {
-                    for y in dest.y..pos.y {
-                        to_check.insert(v(pos.x, y));
-                    }
-                }
-                1 => {
-                    for x in (pos.x + 1)..=dest.x {
-                        to_check.insert(v(x, pos.y));
-                    }
-                }
-                2 => {
-                    for y in (pos.y + 1)..=dest.y {
-                        to_check.insert(v(pos.x, y));
-                    }
-                }
-                3 => {
-                    for x in dest.x..pos.x {
-                        to_check.insert(v(x, pos.y));
-                    }
-                }
-                _ => unreachable!(),
-            }
-            pos = dest;
-        } else {
-            match dir {
-                0 => {
-                    for y in 0..pos.y {
-                        to_check.insert(v(pos.x, y));
-                    }
-                }
-                1 => {
-                    for x in (pos.x + 1)..dim.x {
-                        to_check.insert(v(x, pos.y));
-                    }
-                }
-                2 => {
-                    for y in (pos.y + 1)..dim.y {
-                        to_check.insert(v(pos.x, y));
-                    }
-                }
-                3 => {
-                    for x in 0..pos.x {
-                        to_check.insert(v(x, pos.y));
-                    }
-                }
-                _ => unreachable!(),
-            }
-            break;
-        }
-        dir = (dir + 1) & 0b11;
-    }
-
-    to_check.remove(&start);
-
     let mut total = 0;
 
-    for obstr in to_check {
-        let mut remapped_up = HashMap::default();
-        let dir = Vector::s();
-        let mut pos = obstr + dir;
-        let dest = pos;
-        while map.get(pos) == Some(&b'.') {
-            remapped_up.insert(pos, dest);
-            pos += dir;
-        }
+    for obstr in candidates {
+        let remapped_dests = [Vector::s(), Vector::w(), Vector::n(), Vector::e()].map(|dir| {
+            let mut remapped = HashMap::default();
+            let mut pos = obstr + dir;
+            let dest = pos;
+            while map.get(pos) == Some(&b'.') {
+                remapped.insert(pos, dest);
+                pos += dir;
+            }
+            remapped
+        });
 
-        let mut remapped_right = HashMap::default();
-        let dir = Vector::w();
-        let mut pos = obstr + dir;
-        let dest = pos;
-        while map.get(pos) == Some(&b'.') {
-            remapped_right.insert(pos, dest);
-            pos += dir;
-        }
-
-        let mut remapped_down = HashMap::default();
-        let dir = Vector::n();
-        let mut pos = obstr + dir;
-        let dest = pos;
-        while map.get(pos) == Some(&b'.') {
-            remapped_down.insert(pos, dest);
-            pos += dir;
-        }
-
-        let mut remapped_left = HashMap::default();
-        let dir = Vector::e();
-        let mut pos = obstr + dir;
-        let dest = pos;
-        while map.get(pos) == Some(&b'.') {
-            remapped_left.insert(pos, dest);
-            pos += dir;
-        }
-
-        let remapped_dests = [remapped_up, remapped_right, remapped_down, remapped_left];
-        let mut visited = [
-            HashSet::default(),
-            HashSet::default(),
-            HashSet::default(),
-            HashSet::default(),
-        ];
+        let mut visited: [HashSet<Vector>; 4] = Default::default();
 
         let mut dir = 0;
         let mut pos = start;
